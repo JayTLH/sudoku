8d12629c70b1e2736910b288d9802e6749a3e0e7
jason tlh <jasonjdh94@gmail.com>
2020-05-07 16:15:38 -0400

    added playable grid

--- src/App.js
+++ src/App.js
@@ -1,39 +1,28 @@
 import React, { Component } from 'react'
 import './App.scss'
 
-const dummy = [
-  [4, 0, 8, 0, 7, 0, 0, 5, 2],
-  [0, 0, 0, 9, 0, 0, 1, 0, 7],
-  [0, 0, 6, 0, 0, 0, 8, 0, 0],
-  [0, 0, 0, 0, 5, 0, 0, 0, 6],
-  [5, 0, 4, 0, 0, 0, 0, 0, 8],
-  [0, 8, 0, 2, 0, 7, 0, 0, 0],
+const blank = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [9, 0, 0, 0, 0, 0, 3, 0, 0],
-  [0, 2, 1, 3, 0, 5, 9, 0, 0]
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0]
 ]
 
 export default class App extends Component {
   state = {
-    // grid: dummy
-    grid: [
-      [0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    ]
+    grid: blank
   }
 
+  // returns the position of any zeroes
   empty = (grid) => {
-    for (let row = 0; row < grid.length; row++) {
-      for (let col = 0; col < grid[row].length; col++) {
+    for (let row = 0; row < 9; row++) {
+      for (let col = 0; col < 9; col++) {
         if (grid[row][col] === 0) {
-          // returns the position of any zeroes
           return [row, col]
         }
       }
@@ -42,25 +31,27 @@ export default class App extends Component {
     return false
   }
 
+  // checking if number in position is valid
   valid = (grid, num, pos) => {
     const row = pos[0]
     const col = pos[1]
 
     // check row
-    for (let i = 0; i < grid[0].length; i++) {
+    for (let i = 0; i < 9; i++) {
       if (grid[row][i] === num && col !== i) {
         return false
       }
     }
 
     // check column
-    for (let i = 0; i < grid.length; i++) {
+    for (let i = 0; i < 9; i++) {
       if (grid[i][col] === num && row !== i) {
         return false
       }
     }
 
     // check box
+    // defines box position (top left index)
     const box_y = Math.floor(row / 3) * 3
     const box_x = Math.floor(col / 3) * 3
     for (let y = box_y; y < box_y + 3; y++) {
@@ -88,9 +79,11 @@ export default class App extends Component {
     for (let i = 1; i < 10; i++) {
       if (this.valid(grid, i, [row, col])) {
         grid[row][col] = i
+        // checks to see if the new grid works
         if (this.solve(grid)) {
           return true
         }
+        // if not, revert back to a version that does work
         grid[row][col] = 0
       }
     }
@@ -98,45 +91,99 @@ export default class App extends Component {
     return false
   }
 
+  play = (grid, difficulty) => {
+    // randomize a completed grid
+    const num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    for (let i = 0; i < 9; i++) {
+      const ran = Math.floor(Math.random() * num.length)
+      grid[0][i] = num[ran]
+      num.splice(ran, 1)
+    }
+    this.solve(grid)
+
+    // easy 45 cells, medium 50, hard 60, extreme 65
+    const cells = []
+    while (cells.length !== difficulty) {
+      const ran_y = Math.floor(Math.random() * 9)
+      const ran_x = Math.floor(Math.random() * 9)
+      const exist = cells.find(find => JSON.stringify(find) === JSON.stringify([ran_y, ran_x]))
+      if (!exist) {
+        cells.push([ran_y, ran_x])
+      }
+    }
+
+    cells.forEach(cell => {
+      grid[cell[0]][cell[1]] = ""
+    })
+  }
+
   click = (e) => {
-    this.solve(this.state.grid)
-    this.setState({grid: this.state.grid})
+    switch (e.target.value) {
+      case "solve":
+        this.solve(this.state.grid)
+        this.setState({ grid: this.state.grid })
+        break
+      case "easy":
+        this.setState({ grid: blank }, () => {
+          this.play(this.state.grid, 45)
+          this.setState({ grid: this.state.grid })
+        })
+        break
+      case "medium":
+        this.play(this.state.grid, 50)
+        this.setState({ grid: this.state.grid })
+        break
+      case "hard":
+        this.play(this.state.grid, 55)
+        this.setState({ grid: this.state.grid })
+        break
+      case "extreme":
+        this.play(this.state.grid, 60)
+        this.setState({ grid: this.state.grid })
+        break
+      default:
+        console.log("something was clicked")
+    }
+  }
+
+  change = (e) => {
+    const pos = e.target.id.split("_")
+    const row = pos[1]
+    const col = pos[3]
+    const clone = this.state.grid.slice()
+    clone[row][col] = Number(e.target.value)
+    this.setState({ grid: clone })
   }
 
   render() {
     return (
-      // <div className="app">
-      //   {this.state.grid.map((row, ri) => {
-      //     return (
-      //       <div key={`row${ri}`} style={{ display: "flex" }}>
-      //         {row.map((num, ni) => {
-      //           return (
-      //             <div key={`${num}${ni}`} style={num !== 0 ? { marginRight: "14px" } : { marginRight: "14px", opacity: 0.4 }}>
-      //               {num}
-      //             </div>
-      //           )
-      //         })}
-      //       </div>
-      //     )
-      //   })}
-      //   <button onClick={this.click}>solve</button>
-      // </div>
-
       <div className="app">
         {this.state.grid.map((row, ri) => {
           return (
-            <div key={`row${ri}`} style={{ display: "flex" }}>
-              {row.map((num, ni) => {
+            <div key={`row${ri}`} className="app__row">
+              {row.map((num, ci) => {
                 return (
-                  <div key={`${num}${ni}`} style={num !== 0 ? { marginRight: "14px" } : { marginRight: "14px", opacity: 0.4 }}>
-                    {num}
-                  </div>
+                  <input
+                    key={`${num}${ci}`}
+                    className="app__num"
+                    id={`row_${ri}_col_${ci}`}
+                    name={`row_${ri}_col_${ci}`}
+                    type="number"
+                    value={this.state.grid[ri][ci] === 0 ? "" : this.state.grid[ri][ci]}
+                    onChange={this.change}
+                  />
                 )
               })}
             </div>
           )
         })}
-        <button onClick={this.click}>solve</button>
+        <div className="app__play-btns">
+          <button className="app__btn" onClick={this.click} value="easy">EASY</button>
+          <button className="app__btn" onClick={this.click} value="medium">MEDIUM</button>
+          <button className="app__btn" onClick={this.click} value="hard">HARD</button>
+          <button className="app__btn" onClick={this.click} value="extreme">EXTREME</button>
+        </div>
+        <button className="app__btn" onClick={this.click} value="solve">SOLVE</button>
       </div>
     )
   }
